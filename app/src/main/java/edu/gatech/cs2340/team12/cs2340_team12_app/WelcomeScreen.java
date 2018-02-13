5fafb5b4f2cde30bb3b6462fdb03f7d1e7ac5922
Bryan Kim <bryankim1109@gmail.com>
2018-02-12 20:03:14 -0500

    Created Login and Cancel buttons and linked them to the Login Screen and Welcome Screen respectively

--- app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/WelcomeScreen.java
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/WelcomeScreen.java
@@ -2,12 +2,32 @@ package edu.gatech.cs2340.team12.cs2340_team12_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 
 public class WelcomeScreen extends AppCompatActivity {
-
+    private Button LaunchActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        LaunchActivity = (Button) findViewById(R.id.button);
+        LaunchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity();
+            }
+        });
+
     }
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, LoginScreen.class);
+        startActivity(intent);
+    }
+
 }
