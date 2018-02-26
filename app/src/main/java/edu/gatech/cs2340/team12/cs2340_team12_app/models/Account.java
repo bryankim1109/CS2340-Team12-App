87eb806d6baa186742582c4405aa7c11b52a99a0
Gabriel <gdarosa3@gatech.edu>
2018-02-25 22:29:17 -0500

    Added Account class

---
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/Account.java
@@ -0,0 +1,15 @@
+package edu.gatech.cs2340.team12.cs2340_team12_app.models;
+
+/**
+ * Created by Gabriel on 2/25/2018.
+ */
+
+public abstract class Account {
+
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String email;
+    private Boolean lockStatus;
+
+}
