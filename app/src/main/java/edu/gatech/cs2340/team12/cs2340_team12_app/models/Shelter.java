145824fdc1b6f854f3ab0d01ae5a0cd1afdbe373
Bryan Kim <bryankim1109@gmail.com>
2018-02-25 22:16:10 -0500

    Created Shelter Class

---
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/Shelter.java
@@ -0,0 +1,16 @@
+package edu.gatech.cs2340.team12.cs2340_team12_app.models;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+/**
+ * Created by Bryan on 2/25/2018.
+ */
+
+public class Shelter {
+    private DatabaseReference mDatabase;
+
+
+}
