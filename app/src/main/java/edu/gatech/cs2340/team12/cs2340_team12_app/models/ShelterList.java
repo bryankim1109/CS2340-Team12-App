bb6dc70619f2603ea0e5c00f13c32187884fac9e
calebtkim <calebtkim@gmail.com>
2018-02-27 22:11:22 -0500

    Added ShelterList class
    updated Shelter class as well.

--- app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/ShelterList.java
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/ShelterList.java
@@ -1,10 +1,52 @@
 package edu.gatech.cs2340.team12.cs2340_team12_app.models;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.opencsv.CSVReader;
+
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+/**
+ * Created by caleb on 2/27/2018.
+ */
+
+public class ShelterList {
+
+    private DatabaseReference mDatabase;
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference ref = database.getReference(
+            "server/saving-data/shelterinfo");
+    private List<Shelter> shelters = new ArrayList<>();
+    private DatabaseReference sheltersRef = ref.child("Shelters");
+
+    public void parse(String csvFileName) throws Exception
+    {
+        //Build reader instance
+        //Default seperator is comma
+        //Default quote character is double quote
+        //Start reading from line number 2 (line numbers start from zero)
+        CSVReader reader = new CSVReader(new FileReader(csvFileName + ".csv"));
+
+        //Read CSV line by line and use the string array as you want
+        String[] nextLine = reader.readNext();
+        while (nextLine != null) {
+            if (nextLine != null) {
+                shelters.add(new Shelter(nextLine[1], nextLine[2], nextLine[3],
+                        nextLine[4], nextLine[5], nextLine[6], nextLine[8]));
+            }
+            nextLine = reader.readNext();
+        }
+    }
+}
+package edu.gatech.cs2340.team12.cs2340_team12_app.models;
+
+        import java.io.FileReader;
+        import java.util.ArrayList;
+        import java.util.HashMap;
+        import java.util.List;
+        import java.util.Map;
 
 /**
  * Created by Bryan on 2/25/2018.
@@ -90,3 +132,4 @@ public class Shelter {
     }
 
 }
+
