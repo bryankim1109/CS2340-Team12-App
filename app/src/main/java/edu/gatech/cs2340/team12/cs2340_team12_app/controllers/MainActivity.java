47fb0a58dd2bfa138f531dff39e1afd2c8465ab4
Gabriel <gdarosa3@gatech.edu>
2018-03-02 17:04:01 -0500

    now pulling shelters from the database

--- app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/controllers/MainActivity.java
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/controllers/MainActivity.java
@@ -10,7 +10,14 @@ import android.view.View;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.io.FileNotFoundException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.gatech.cs2340.team12.cs2340_team12_app.R;
 import edu.gatech.cs2340.team12.cs2340_team12_app.models.CSVParser;
@@ -18,30 +25,56 @@ import edu.gatech.cs2340.team12.cs2340_team12_app.models.Shelter;
 import edu.gatech.cs2340.team12.cs2340_team12_app.models.ShelterList;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference databaseShelters;
+    List<Shelter> shelterList;
+    Spinner shelterSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(false) {
-            try {
-                CSVParser.parse(getResources().openRawResource(R.raw.homeless_shelter_database));
+//        if(false) {
+//            try {
+//                CSVParser.parse(getResources().openRawResource(R.raw.homeless_shelter_database));
+//            }
+//            catch(Exception a){
+//                Toast.makeText(MainActivity.this, "Parser doesn't work", Toast.LENGTH_LONG).show();
+//            }
+//        }
+
+        databaseShelters = FirebaseDatabase.getInstance().getReference("Shelters");
+        shelterList = new ArrayList<>();
+        databaseShelters.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot shelterSnapshot : dataSnapshot.getChildren()) {
+                    Shelter shelter = shelterSnapshot.getValue(Shelter.class);
+
+                    shelterList.add(shelter);
+                }
+
+                Toast.makeText(MainActivity.this, "Database read", Toast.LENGTH_LONG).show();
+                shelterSpinner = findViewById(R.id.shelterSpinner);
+                ArrayAdapter<Shelter> adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item, shelterList);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                shelterSpinner.setAdapter(adapter);
             }
-            catch(Exception a){
-                Toast.makeText(MainActivity.this, "Parser doesn't work", Toast.LENGTH_LONG).show();
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
-        }
+        });
+
+
+        shelterSpinner = findViewById(R.id.shelterSpinner);
+        ArrayAdapter<Shelter> adapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item, shelterList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        shelterSpinner.setAdapter(adapter);
 
         Button LogOut;
         Button Select;
-        final Spinner shelterSpinner;
-
-        ShelterList shelterList = new ShelterList();
-
-        shelterSpinner = findViewById(R.id.shelterSpinner);
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item, shelterList.getShelters());
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        shelterSpinner.setAdapter(adapter);
 
         LogOut = findViewById(R.id.logout);
         LogOut.setOnClickListener(new View.OnClickListener() {
