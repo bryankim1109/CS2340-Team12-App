b2bd710e63fc417915f69dc08ab329d2c22a36dd
eddychiao <eddychiao@gmail.com>
2018-03-05 22:54:09 -0500

    made an enum to populate gender spinner

---
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/Gender.java
@@ -0,0 +1,9 @@
+package edu.gatech.cs2340.team12.cs2340_team12_app.models;
+
+/**
+ * Created by eddychiao on 3/5/18.
+ */
+
+public enum Gender {
+    MALE, FEMALE, AGENDER, GENDERFLUID, NONCONFORMANT, TRANSGENDER, OTHER
+}
