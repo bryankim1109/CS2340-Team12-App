49e17a3730264d4fac344d1cf8929c095a443b52
eddychiao <eddychiao@gmail.com>
2018-03-26 22:01:56 -0400

    checks for null case

--- app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/ShelterList.java
+++ app/src/main/java/edu/gatech/cs2340/team12/cs2340_team12_app/models/ShelterList.java
@@ -27,25 +27,26 @@ public class ShelterList {
 
     public void filterShelters(User user, String name) {
         filteredShelters.clear();
-        String userGender = user.getGender();
-        String userAgeGroup = user.getAgeGroup();
-        for(Shelter shelter : allShelters) {
-            String gender = shelter.getGender();
-            String ageGroup = shelter.getAgeGroup();
-            if(userGender.equals("Anyone") || gender.equals("Anyone") || (userGender.equals("Male") && gender.contains("Men")) ||
-                    (userGender.equals("Female") && gender.contains("Women"))) {
+        if (user != null && name != null) {
+            String userGender = user.getGender();
+            String userAgeGroup = user.getAgeGroup();
+            for (Shelter shelter : allShelters) {
+                String gender = shelter.getGender();
+                String ageGroup = shelter.getAgeGroup();
+                if (userGender.equals("Anyone") || gender.equals("Anyone") || (userGender.equals("Male") && gender.contains("Men")) ||
+                        (userGender.equals("Female") && gender.contains("Women"))) {
 
-                if (userAgeGroup.equals("Anyone") || ageGroup.equals("Anyone") ||(userAgeGroup.equals("Young Adults") && ageGroup.contains("Young Adults")) ||
-                        (userAgeGroup.equals("Family with newborns") && ageGroup.contains("Families")) ||
-                        (userAgeGroup.equals("Children") && ageGroup.contains("Children"))) {
+                    if (userAgeGroup.equals("Anyone") || ageGroup.equals("Anyone") || (userAgeGroup.equals("Young Adults") && ageGroup.contains("Young Adults")) ||
+                            (userAgeGroup.equals("Family with newborns") && ageGroup.contains("Families")) ||
+                            (userAgeGroup.equals("Children") && ageGroup.contains("Children"))) {
 
-                    if(name.equals("") || name.equals("Shelter Name") || name.equals(shelter.getShelterName())) {
-                        filteredShelters.add(shelter);
+                        if (name.equals("") || name.equals("Shelter Name") || name.equals(shelter.getShelterName())) {
+                            filteredShelters.add(shelter);
+                        }
                     }
                 }
             }
         }
-
     }
 
 }
